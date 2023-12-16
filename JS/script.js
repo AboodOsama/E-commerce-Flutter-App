86a1f5449830d2a351fa9254863ed0437093d7ea
Abdulla <abdullaalqatta@gmail.com>
2023-12-16 06:11:29 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,50 @@
+    
+    
+    // --password==77776;
+    // var user=new person("Abdulla",77776);
+    let person=prompt("Please Enter your name ","") ;
+    let password= prompt("enter yuor password","");
+    // if(person!=null)
+    // {
+       
+    // }
+     // document.getElementById("dem").innerHTML="Hello "+person+" How are you Today? \n";
+     if(password==77776)
+     {
+         document.getElementsByName("link");  
+     } 
+     else if(password!=77776)
+     {
+         confirm(person+" your password is wrong Do want to try agin \n ")
+         prompt("enter yuor password","");
+         if(password==77776)
+         {
+            document.getElementsByName("link"); 
+         }
+         else
+         {
+            close();
+         }
+    }
+    else{
+        // alert("Your password is wrong");
+        close();
+    }
+
+    // EX2
+    function mo(){
+		document.getElementById("imgs").style.height= '400px';
+		}
+		function mb(){
+		document.getElementById("imgs").style.height= '250px';
+		}
+        function co(){
+            document.getElementById("co").style.hover
+        }
+        function myfunction(){
+            document.getElementById("texts").innerHTML
+        }
+    
+    
+    
+    
